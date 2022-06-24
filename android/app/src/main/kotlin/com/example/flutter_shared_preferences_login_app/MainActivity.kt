69617f37e98308990a5020259e86cd1e266a2657
Taha Turan <tahaaturann16@gmail.com>
2022-06-24 03:33:28 +0300

    Shared Preferences Login App

---
+++ android/app/src/main/kotlin/com/example/flutter_shared_preferences_login_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_shared_preferences_login_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shared_preferences_login_app/views/my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
